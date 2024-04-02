0b809c56094da8bef7603328e824955958784a6b
myca03 <myca03@icloud.com>
2024-04-02 18:55:33 -0400

    Sidebar Created!

--- react-login/frontend/src/App.js
+++ react-login/frontend/src/App.js
@@ -36,6 +36,7 @@ function App() {
 
   return (
     <div className="App">
+      <Sidebar pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home email={email}/>} />
