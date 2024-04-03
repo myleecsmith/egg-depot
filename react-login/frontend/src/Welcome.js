70c0d19c35a6738fd0ffe4c3a3fb461af8862100
myca03 <myca03@icloud.com>
2024-04-02 21:26:25 -0400

    Added sound, changed the background!

--- react-login/frontend/src/Welcome.js
+++ react-login/frontend/src/Welcome.js
@@ -1,13 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Sidebar from './sidebar'; // Assuming Sidebar component is in the same directory
 import Button from './button'; // Assuming Button component is in the same directory
 import './App.css'; // Assuming you have a CSS file for styling
+import sound from './WelcomeSounds.wav';
 
 const Welcome = (props) => {
-  const { loggedIn, email } = props;
+  const { loggedIn } = props;
   const navigate = useNavigate();
   const [isOpen, setIsOpen] = useState(false);
+  const [isAudioPlaying, setIsAudioPlaying] = useState(false);
+
+  useEffect(() => {
+    const isAudioPlayingStr = localStorage.getItem('isAudioPlaying');
+    const isAudioPlaying = isAudioPlayingStr === 'true';
+    setIsAudioPlaying(isAudioPlaying);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('isAudioPlaying', isAudioPlaying.toString());
+  }, [isAudioPlaying]);
 
   const toggleSidebar = () => {
     setIsOpen(!isOpen);
@@ -17,17 +29,39 @@ const Welcome = (props) => {
     navigate('/');
   };
 
+  const toggleAudio = () => {
+    setIsAudioPlaying(!isAudioPlaying);
+  };
+
   return (
     <div className="welcome-page">
+      {/* Adding audio element for WelcomeSounds */}
+      {isAudioPlaying && (
+        <audio autoPlay loop>
+          <source src={sound} type="audio/wav" />
+          Your browser does not support the audio element.
+        </audio>
+      )}
+      {/* Button to toggle audio */}
+      <button onClick={toggleAudio}>
+        {isAudioPlaying ? 'Pause Audio' : 'Play Audio'}
+      </button>
+      {/* Button to toggle sidebar */}
       <Button onClick={toggleSidebar} />
+      {/* Sidebar component */}
       <Sidebar isOpen={isOpen} toggle={toggleSidebar} />
+      {/* Main content */}
       <div className="home-bg">
         <div className="mainContainer">
+          <div className="Flag1">
+            <img src={require('./images/flag.gif')} alt="ducky" height={200} />
+          </div>
           <div className={'titleContainer'}>
             <div className={'buttonContainer'}>
+              {/* Buttons */}
               <input
                 className={'inputButton'}
-                type="island1"
+                type="button"
                 onClick={onButtonClick}
                 value={loggedIn ? 'Island 1' : 'Log in'}
               />
@@ -37,7 +71,6 @@ const Welcome = (props) => {
                 onClick={onButtonClick}
                 value={loggedIn ? 'Log out' : 'Log in'}
               />
-              <img src='./images/duck.jpeg' alt='ducky'/>
             </div>
           </div>
         </div>
